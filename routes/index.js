054de2cbf087c374d26ba5306377c0e6b7e79e30
Ben Jenkins <benjenkinsv95@gmail.com>
2018-08-01 20:19:38 -0400

    Fixing home redirect.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.redirect('http://example.com');
  res.redirect('https://github.com/benjenkinsv95/barliman-api')
 });
 
